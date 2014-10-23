d692669f99b911d1930674e8c69034b68641342d
adowns01 <downs.amelia@gmail.com>
2014-10-22 18:01:27 -0700

    added game-controller moved spaqhetti funtions into it

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
-	$(window).keydown(move);
+	
+	my_game_controller = new gameController()
+	my_game_controller.setUpEventListeners();
+	setTimeout(my_game_controller.loadIntro, 50);
 	setUpItems();
-	setTimeout(introScreen, 50);
 })
-
-
-
-
-
 
 IN_STARTUP_SCREEN = false;
 IN_MAIN_PLAY = false;
@@ -31,6 +28,64 @@ ITEM_PATHS = ['stick.png', 'marble.png', 'seeds.png', 'feather.png', 'bottle-cap
 ITEMS_ON_PAGE = []
 
 
+function gameController(){
+	this.in_startup_screen = false;
+	this.in_main_game_play = false;
+}
+
+gameController.prototype = {
+	loadIntro: function(){
+		CANVAS.drawImage(STARTUP_SCREEN, 0, 0, 700, 300)
+		this.in_startup_screen = true;
+		IN_STARTUP_SCREEN = true;
+	}, 
+	loadMainPlay: function(){
+		IN_MAIN_PLAY = true;
+		this.in_main_game_play = true;
+		drawBackground();
+		fujiView.draw1(0,0);
+	}, 
+	setUpEventListeners: function(){
+		$(window).keydown(this.determineKeyPressAction.bind(this));
+	}, 
+	determineKeyPressAction: function(e){
+		if (IN_STARTUP_SCREEN){
+			if (e.keyCode == 13){
+				IN_STARTUP_SCREEN = false;
+				this.loadMainPlay();
+			}
+		}
+		else if (!backpack.is_open || e.keyCode == 90){
+			switch (e.keyCode){
+			case 37:// left arrow
+			fuji.moveLeft();
+			break;
+			case 39: //right arrow
+			fuji.moveRight();
+			break;
+			case 38: //up arrow
+			fuji.moveUp();
+			break;
+			case 40: //down arrow
+			fuji.moveDown();
+			break;
+			case 90: // z
+			backpack.toggle();
+			break;
+			case 32: // space
+			putItemRandomlyOnPage();
+			break;
+		}
+	}
+}
+}
+
+
+
+
+
+
+
 function putItemRandomlyOnPage(){
 	y = Math.random() * 250
 	x = Math.random() * 650
@@ -46,10 +101,6 @@ function putItemRandomlyOnPage(){
 	ITEMS_ON_PAGE.push(displayed_item)
 }
 
-function introScreen(){
-	CANVAS.drawImage(STARTUP_SCREEN, 0, 0, 700, 300)
-	IN_STARTUP_SCREEN = true;
-}
 
 function setUpItems(){
 	for(var i = 0; i<ITEM_PATHS.length; i++){
@@ -63,12 +114,6 @@ function addItems(url){
 	ITEMS.push(newItem)
 }
 
-
-function loadMainPLay(){
-	IN_MAIN_PLAY = true;
-	drawBackground();
-	fujiView.draw1(0,0);
-}
 
 function drawBackground(){
 	CANVAS.drawImage(BACKGROUND, 0, 0, 700, 300)
@@ -97,37 +142,5 @@ function drawBackpack(){
 	drawBackpackItems();
 }
 
-
-
-function move(e){
-	if (IN_STARTUP_SCREEN){
-		if (e.keyCode == 13){
-			IN_STARTUP_SCREEN = false;
-			loadMainPLay();
-		}
-	}
-	else if (!backpack.is_open || e.keyCode == 90){
-		switch (e.keyCode){
-			case 37:// left arrow
-			fuji.moveLeft();
-			break;
-			case 39: //right arrow
-			fuji.moveRight();
-			break;
-			case 38: //up arrow
-			fuji.moveUp();
-			break;
-			case 40: //down arrow
-			fuji.moveDown();
-			break;
-			case 90: // z
-			backpack.toggle();
-			break;
-			case 32: // space
-			putItemRandomlyOnPage();
-			break;
-		}
-	}
-}
 
 
